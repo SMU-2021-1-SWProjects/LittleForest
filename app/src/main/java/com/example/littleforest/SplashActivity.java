906115d582660bff8b6109f298ad919735c1b0bc
CSeoyeon <choiseoyeon0223@gmail.com>
2021-05-27 15:11:25 +0900

    user health imformation input

---
+++ app/src/main/java/com/example/littleforest/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.littleforest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+public class SplashActivity extends Activity {
+
+    protected void onCreate(Bundle saveInstanceState)
+    {
+        super.onCreate(saveInstanceState);
+        try{
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        Intent page = new Intent(this, MainActivity.class);
+
+        startActivity(page);
+        finish();
+
+    }
+
+}
